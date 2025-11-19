5ea2e6bb38da828efaaef643078373c4eeef5a6e
Yiğit <durakyigit10@gmail.com>
2025-11-19 22:48:01 +0300

    initial commit

---
+++ filter.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func cencored(s string) any {
+	sum := 0
+	cencoredList := "şçğüöim"
+	for i := 0; i <= len(s)-1; i++ {
+		for x := 0; x <= len(cencoredList)-1; x++ {
+			if string(cencoredList[x]) == string(s[i]) {
+				sum = 1
+				break
+			}
+		}
+
+	}
+	if sum > 0 {
+		fmt.Println("Yasaklı karakter algılandı")
+	} else {
+		fmt.Println("Başarılı")
+	}
+	return 0
+}
+
+func main() {
+	cencored("kaola")
+}
